647b746a2e2384915151aaf50b8e0b232e5eeca6
Derik <dsalazar35214@ucq.edu.mx>
2025-05-14 16:12:00 -0600

    Pagina de presentacion

---
+++ resources/views/andesa.blade.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>MULTISISTEMAS ANDESA - LA MEJOR TECNOLOGÍA</title>
+    <link rel="stylesheet" href="{{ asset('assets/styles.css') }}">
+</head>
+<body>
+<div class="card" style="margin:20px;">
+    <div class="card-header fondo_andesa text-center">
+        <h1 class="text-danger">MULTISISTEMAS ANDESA</h1>
+        <div class="label-danger">
+            <h3>MÁS DE 30 AÑOS</h3>
+            <p>DE EXPERIENCIA</p>
+        </div>
+    </div>
+    <div class="card-body text-primary">
+        <div class="row">
+            <div class="col-lg-6">
+                <img src="{{ asset('assets/images/logo_andesa.png') }}" alt="Logo Andesa" class="logo-andesa">
+            </div>
+            <div class="col-lg-6">
+                <ul class="services-list">
+                    <li>Asesor de sistemas</li>
+                    <li>Reparación de equipo de cómputo Microsoft y Mac</li>
+                    <li>Dominios</li>
+                    <li>Hosting</li>
+                    <li>Redes inalámbricas</li>
+                    <li>Toners de todas las marcas</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div class="card-footer fondo_andesa">
+        <div class="row">
+            <div class="col text-left">
+                <p>Oficina <b>868 825 22 22</b></p>
+                <p>Whatsapp <a href="https://wa.me/5218688947229" target="_blank"><b>868 894 72 29</b></a></p>
+            </div>
+            <div class="col text-right">
+                <p><a href="mailto:oficina@andesa.mx">oficina@andesa.mx</a></p>
+                <p>1a y Matamoros #1311</p>
+            </div>
+        </div>
+    </div>
+</div>
+</body>
+</html>
